fd20d3359f915645765d0eaa452c634f1eddcc24
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2020-07-05 01:00:19 +0200

    initial author

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ if(process.env.NODE_ENV !=='production'){
 const express=require('express')
 const app=express()
 const expressLayouts=require('express-ejs-layouts')
+const bodyParser=require('body-parser')
 
 
 
 const indexRouter=require('./routes/index')
+const authorRouter=require('./routes/authors')
 
 
 app.set('view engine','ejs')
@@ -19,6 +21,7 @@ app.set('views',__dirname+'/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
 
 
 const mongoose=require('mongoose')
@@ -31,6 +34,7 @@ db.on('error',error=>console.error(error))
 db.once('open',()=>console.log('Conected to mongoose'))
 
 app.use('/',indexRouter)
+app.use('/authors',authorRouter)
 
 
 app.listen(process.env.PORT || 3000)
